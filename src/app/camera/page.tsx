46a29ce92bad97fceb05d4129ff91764861441d9
enkhamgalan <enkhamgalan.ba@mobicom.mn>
2025-06-30 16:25:45 +0800

    Egg shape

--- src/app/camera/page.tsx
+++ src/app/camera/page.tsx
@@ -1,17 +1,24 @@
 'use client'
 import Webcam from "react-webcam";
 
+interface WebcamRef {
+
+}
+
 export default function CameraPage() {
   return (
-    <div className="flex justify-center items-center h-screen">
-        <div className="w-full max-w-[640px] aspect[4/3] relative">
-            <Webcam
-                style={{
-                    transform: 'scaleX(-1)',
-                }}
-            />
-        </div>
+    <div className="flex justify-center items-center h-screen relative">
+      <div className="relative flex justify-center items-center">
+        <Webcam
+          className="w-72 h-96 rounded-full object-cover border-4 border-white shadow-lg bg-black"
+          style={{
+            transform: 'scaleX(-1)',
+            borderRadius: '60% 60% 70% 70% / 55% 55% 80% 80%',
+          }}
+        />
+
+      </div>
+
     </div>
   );
 }
-
